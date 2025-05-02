302d074975c22a05dea8fda18b7cb4ec278908ed
andrea-delaflor <andrea.delaflor@alumnos.upm.es>
2025-05-02 14:25:43 +0200

    vector 2d

---
+++ Ajedrez/JuegoAjedrez/src/Vector2D.h
@@ -0,0 +1,62 @@
+#pragma once
+#include<cmath>
+
+//Utilizadades de la clase Vector 2D en nuestro ajedrez
+//1.Representar la posición de las piezas en el tablero mediante un par de números reales (x, y).
+//2.Facilitar los cálculos relacionados con el movimiento de las piezas, por ejemplo :
+//3.Distancia entre dos posiciones(usando modulo()).
+//4.Dirección de un movimiento(usando unitario()).
+//5.Verificar movimientos legales o trayectorias(con suma, resta, y producto escalar)
+struct Vector2D
+{
+	double x{};
+	double y{};
+	double modulo() const;						 //modulo del vector
+	double argumento() const;					 //argumento del vector
+	Vector2D unitario() const;					 //devuelve un vector unitario
+	Vector2D operator - (const Vector2D&) const; //resta de vectores
+	Vector2D operator + (const Vector2D&) const; //suma de vectores
+	double operator *(const Vector2D&) const;    // producto escalar
+	Vector2D operator *(double) const;           // producto por un escalar
+};
+
+inline double Vector2D::modulo() const
+{
+	return sqrt(x * x + y * y);
+}
+
+inline double Vector2D::argumento() const
+{
+	return atan2(y, x);
+}
+
+inline Vector2D Vector2D::unitario() const
+{
+	auto mod = modulo();
+	if (mod > 0.00001) return { x / mod, y / mod };
+	return { x,y };
+}
+
+inline Vector2D Vector2D::operator - (const Vector2D& v) const
+{
+	return { x - v.x , y - v.y };
+}
+inline Vector2D Vector2D::operator + (const Vector2D& v) const
+{
+	return { x + v.x , y + v.y };
+}
+
+inline double Vector2D::operator *(const Vector2D& v) const
+{
+	return{ x * v.x + y * v.y };
+}
+
+inline Vector2D Vector2D::operator *(double n) const
+{
+	return{ x * n, y * n };
+}
+
+inline Vector2D operator * (double f, const Vector2D& v) {
+	return { v.x * f, v.y * f };
+}
+
