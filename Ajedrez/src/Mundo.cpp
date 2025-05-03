daa33dc423e29b9d2c053738ac8e648da773cc61
lorenzohidalgoanero <lorenzo.hidalgo@alumnos.upm.es>
2025-05-03 21:34:58 +0200

    incluir librerias y funcion inicializa

--- Ajedrez/src/Mundo.cpp
+++ Ajedrez/src/Mundo.cpp
@@ -1,1 +1,14 @@
 #include "Mundo.h"
+#include "freeglut.h"
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+
+
+void Mundo::inicializa() {
+    x_ojo = 0.0;
+    y_ojo = 0.0;
+    z_ojo = 0.0;
+
+}
--- Ajedrez/src/Mundo.h
+++ Ajedrez/src/Mundo.h
@@ -1,6 +1,13 @@
 #pragma once
+#include "Vector2D.h"
+#include "tablero.h"
 
 class Mundo
 {
+
+
+public:
+	void dibuja();
+	void inicializa();
 };
 
