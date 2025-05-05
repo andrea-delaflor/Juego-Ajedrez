ce4e88674ae2fcf8ac2cad6fa597fd8a4b4f05e6
juliamagallares <julia.magallares@alumnos.upm.es>
2025-05-05 17:56:56 +0200

    tablero

--- Ajedrez/src/Tablero.cpp
+++ Ajedrez/src/Tablero.cpp
@@ -1,4 +1,19 @@
 #pragma once
+#include <iostream>
 #include "Tablero.h"
 #include "freeglut.h"
 #include "ETSIDI.h"
+
+
+void Tablero::dibuja()
+{
+
+    //CASILLAS 
+
+    //FONDO 
+}
+
+void Tablero::inicializa(const int& TJ)
+{
+    Tipo_juego = TJ;
+}
--- Ajedrez/src/Tablero.h
+++ Ajedrez/src/Tablero.h
@@ -1,9 +1,27 @@
 #pragma once
 #include "Vector2D.h"
 
-class Tablero
-{
-	Vector2D coordenadas{};
 
+struct CASILLA
+{   
+    // Parámetros gráficos de casilla 
+    int vx;
+    int vy;
+    int colR, colG, colB;
 };
 
+class Tablero
+{
+private:
+
+    int Tipo_juego, Tipo_oponente; // Tipo de juego al que vamos a jugar y tipo de oponente (jugador o maquina) que vamos a enfrentar
+
+    CASILLA Mcasillas[5][4];  // Para parte GRÁFICA -> Indica posición filasxcolumnas
+    Vector2D PosEnCasillas[5][4];  // Para parte GRÁFICA -> Indica posición del centro de las casilas
+    // i = FILAS , j = COLUMNAS         [filas][columnas] 
+public:
+    void inicializa(const int& TJ); //Aqui inicializamos el tipo de juego (4×5 con un único peón o Speed Chess )
+    void dibuja();
+
+};
+      
