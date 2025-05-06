fe0d60d5d3e5261475a9991685c5d092c4f2a376
patridelpozo <patricia.delpozo@alumnos.upm.es>
2025-05-06 17:51:56 +0200

    incio torre.cpp

--- Ajedrez/src/Torre.cpp
+++ Ajedrez/src/Torre.cpp
@@ -1,1 +1,27 @@
 #include "Torre.h"
+#include "ETSIDI.h"
+#include "freeglut"
+#include "Fichas.h"
+
+void Torre::muevepieza(double x, double y)
+{
+	pos_pieza.x = x;
+	pos_pieza.y = y;
+}
+
+void Torre::set_pos_pieza(const Vector2D& pos)
+{
+	if ()
+		pos_pieza = pos;
+}
+
+void Torre::set_color_pieza(bool c)
+{
+	if (c == true)
+		color = true; //blanco
+	if (c == false)
+		color = false; //negro
+}
+//falta la lógica de la torre con el std
+//ver que es lo de piezaahogada y casilla valida
+
