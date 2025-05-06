75d651f77ba8111fa0d9b938d0e62e9057607751
Carlota-pg <carlota.pinedo03@gmail.com>
2025-05-06 12:31:54 +0200

    Empezar Ficha

--- Ajedrez/src/Color.h
+++ Ajedrez/src/Color.h
@@ -1,6 +1,5 @@
 #pragma once
-class Caballo
+class Color
 {
-
 };
 
--- Ajedrez/src/Fichas.h
+++ Ajedrez/src/Fichas.h
@@ -1,5 +1,9 @@
 #pragma once
+#include "Vector2D.h"
+#include "Color.h"
 class Ficha
 {
+	Vector2D posicion{};
+	Color color;
 };
 
