b322ec14219c82f26a9e87281d46fdc7caf3a790
patridelpozo <patricia.delpozo@alumnos.upm.es>
2025-05-06 17:10:10 +0200

    ..

--- Ajedrez/src/Color.h
+++ Ajedrez/src/Color.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "freeglut.h"
-typedef unsigned char uchar;
+typedef unsigned char ucharb; //blanco
+typedef unsigned char ucharn; //negro
 
 class Color
 {
-	uchar r{ 255 }, g{ 255 }, b{ 255 }; //color blanco
+	ucharb r{ 255 }, g{ 255 }, b{ 255 }; //color blanco
+	ucharn r{ 0 }, g{ 0 }, b{ 0 }; //color negro
 	void pon_color() {
 		glColor3ub(r, g, b);
 	}
