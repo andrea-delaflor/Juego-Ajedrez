d13bfc2153913d93eec6f768e68134b28ce49ba6
Carlota-pg <carlota.pinedo03@gmail.com>
2025-05-30 01:33:50 +0200

    reina 4

--- Ajedrez/src/Reina.cpp
+++ Ajedrez/src/Reina.cpp
@@ -51,20 +51,40 @@ bool Reina::FichaAhogada(vector<Vector2D> posiciones)
 
 std::vector<Vector2D> Reina::get_movimientos_validos(std::vector<std::vector<Fichas*>> control, Vector2D pos, Vector2D reyPos)
 {
-    haComidoFicha = false; 
-    std::vector<Vector2D> posiciones; 
-   
+    haComidoFicha = false;
+    std::vector<Vector2D> posiciones;
 
-    /*
-	Los movimientos de la reina
-	.
-	.
-	.
-	.
-	.
-	*/
-	
-   
+    // Movimientos en 8 direcciones: (dx, dy)
+    int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };            //(1,0) == derecha, (-1,0) == izquierda, (0,1) == abajo , (0, -1) == arriba, (1,1) == diagonal abajo derecha......
+    int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+
+    // Recorremos cada dirección
+    for (int dir = 0; dir < 8; dir++) {   // la reina tiene 8 movimientos posibles asi q dir = 8
+        int x = static_cast<int>(pos.x);
+        int y = static_cast<int>(pos.y);
+
+        while (true) {    //haComidoFicha sigue en False......es decir se mueve hasta topar con otra ficha
+            x += dx[dir];
+            y += dy[dir];
+
+            // Comprobamos que la casilla esté dentro del tablero
+            if (!casillaValida(x, y, control))
+                break;
+
+            // Si la casilla está vacía, la añadimos como válida
+            if (control[x][y] == nullptr) {
+                posiciones.push_back({ static_cast<double>(x), static_cast<double>(y) });
+            }
+            else {
+                // Si hay una ficha 
+                posiciones.push_back({ static_cast<double>(x), static_cast<double>(y) });
+                haComidoFicha = true; //se deja de mover
+                
+                break;
+            }
+        }
+    }
+
     return posiciones;
 }
 
