3f6ee7a1bc582c72f2747c189f078a9de391aab7
Carlota-pg <carlota.pinedo03@gmail.com>
2025-05-29 22:36:27 +0200

    empezando reina

---
+++ Ajedrez/src/Reina.cpp
@@ -0,0 +1,32 @@
+#include "Reina.h"
+
+void Reina::mueveficha(double, double)
+{
+}
+
+void Reina::set_pos_ficha(const Vector2D& pos)
+{
+}
+
+void Reina::dibuja_ficha()
+{
+}
+
+void Reina::set_color_ficha(bool a)
+{
+}
+
+bool Reina::FichaAhogada(vector<Vector2D> posiciones)
+{
+	return false;
+}
+
+std::vector<Vector2D> Reina::get_movimientos_validos(std::vector<std::vector<Fichas*>> control, Vector2D pos, Vector2D reyPos)
+{
+	return std::vector<Vector2D>();
+}
+
+bool Reina::casillaValida(int i, int j, std::vector<std::vector<Fichas*>> control)
+{
+	return false;
+}
--- Ajedrez/src/Reina.h
+++ Ajedrez/src/Reina.h
@@ -1,6 +1,41 @@
 #pragma once
-class Reina
+#include <vector>
+#include "ETSIDI.h"
+#include "Vector2D.h"
+#include "Fichas.h"
+
+using std::vector;
+
+
+class Reina : public Fichas    //heredamos
 {
+	bool color;
+	bool haComidoFicha;
+
+	ETSIDI::SpriteSequence sprite{ "imagenes/REINA BLANCA.png", 1 };
+	ETSIDI::SpriteSequence sprite2{ "imagenes/REINA NEGRA.png", 1 };
+
+public:
+	Reina() {
+		sprite.setCenter(+0.75, +0.75);
+		sprite.setSize(1.5, 1.5);
+		sprite2.setCenter(+0.75, +0.75);
+		sprite2.setSize(1.5, 1.5);
+		color = true; // Blanco predeterminado
+	}
+
+	void mueveficha(double, double) override;
+	void set_pos_ficha(const Vector2D& pos) override;
+	void dibuja_ficha() override;
+	void set_color_ficha(bool a) override;    //recuerda TRUE= blanco......FALSE=negro
+	bool get_color() override {return color;}
+	bool get_haComidoFicha() override {return haComidoFicha;}
+	Vector2D get_pos() override {return posicion_ficha;}
+
+	bool FichaAhogada(vector<Vector2D> posiciones);
+	std::vector<Vector2D> get_movimientos_validos(std::vector<std::vector<Fichas*>> control, Vector2D pos, Vector2D reyPos) override;
+
+	bool casillaValida(int i, int j, std::vector<std::vector<Fichas*>> control) override;
 
 };
 
