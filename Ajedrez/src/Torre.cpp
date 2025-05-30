85b85558305d533bd1e175d992418bf292885cc4
Carlota-pg <carlota.pinedo03@gmail.com>
2025-05-30 22:37:53 +0200

    Empezando Torre

--- Ajedrez/src/Torre.cpp
+++ Ajedrez/src/Torre.cpp
@@ -1,27 +1,41 @@
-/*#include "Torre.h"
+#include "Torre.h"
+#include "freeglut.h"
 #include "ETSIDI.h"
-#include "freeglut"
+#include "Mundo.h"
 #include "Fichas.h"
+#include <stdio.h>
 
-void Torre::muevepieza(double x, double y)
+void Torre::mueveficha(double, double)
 {
-	pos_pieza.x = x;
-	pos_pieza.y = y;
+
 }
 
-void Torre::set_pos_pieza(const Vector2D& pos)
+void Torre::set_pos_ficha(const Vector2D& pos)
 {
-	if ()
-		pos_pieza = pos;
+
 }
 
-void Torre::set_color_pieza(bool c)
+void Torre::dibuja_ficha()
 {
-	if (c == true)
-		color = true; //blanco
-	if (c == false)
-		color = false; //negro
+
 }
-//falta la lógica de la torre con el std
-//ver que es lo de piezaahogada y casilla valida
-*/
+
+void Torre::set_color_ficha(bool a)
+{
+
+}
+
+bool Torre::FichaAhogada(vector<Vector2D> posiciones)
+{
+	return false;
+}
+
+std::vector<Vector2D> Torre::get_movimientos_validos(std::vector<std::vector<Fichas*>> control, Vector2D pos, Vector2D reyPos)
+{
+	return std::vector<Vector2D>();
+}
+
+bool Torre::casillaValida(int i, int j, std::vector<std::vector<Fichas*>> control)
+{
+	return false;
+}
--- Ajedrez/src/Torre.h
+++ Ajedrez/src/Torre.h
@@ -1,32 +1,48 @@
-/*#pragma once
+#pragma once
 #include "ETSIDI.h"
 #include "Fichas.h"
+#include <vector>
+#include "Vector2D.h"
+
+
+using std::vector;
+
+
 
 class Torre:public Fichas
 {
-private:
 	bool color; //al usar bool se devuelve un valor booleano, es decir, true o false
+	bool haComidoFicha;
+
+	ETSIDI::SpriteSequence sprite{ "imagenes/TORRE BLANCA.png", 1 };
+	ETSIDI::SpriteSequence sprite2{ "imagenes/TORRE NEGRA.png", 1 };
 
 public:
 	Torre() 
 	{
-		//aqui van las sentencias del sprite
-		color = false; //negro
+		sprite.setCenter(+0.75, +0.75);
+		sprite.setSize(1.5, 1.5);
+		sprite2.setCenter(+0.75, +0.75);
+		sprite2.setSize(1.5, 1.5);
+		color = true; // Blanco predeterminado
 	}
-	void muevepieza(double, double);
-	void dibuja_pieza();
-	void set_color_pieza(bool c);
-	bool get_color()
-	{
-		return color;
-	}
-	void set_pos_pieza(const Vector2D& pos);
-	Vector2D get_pos()
-	{
-		return pos_pieza;
-	}
-	//entender que es lo de piezaahogada y casillavalida
-	// casilla valida creo que son las casillas que son posibles movimientos teniendo en cuenta como se mueve la ficha
-	//ver tambien la lógica de de la eliminacion de piezas: como se come una pieza la torre
+
+	void mueveficha(double, double) override;
+	void set_pos_ficha(const Vector2D& pos) override;
+	void dibuja_ficha() override;
+	void set_color_ficha(bool a) override;    //recuerda TRUE= blanco......FALSE=negro
+	bool get_color() override { return color; }
+	bool get_haComidoFicha() override { return haComidoFicha; }
+	Vector2D get_pos() override { return posicion_ficha; }
+
+	bool FichaAhogada(vector<Vector2D> posiciones);
+	std::vector<Vector2D> get_movimientos_validos(std::vector<std::vector<Fichas*>> control, Vector2D pos, Vector2D reyPos) override;
+
+	bool casillaValida(int i, int j, std::vector<std::vector<Fichas*>> control) override;
 };
+
+
+
+
+
 
