69cac781169a8eff6b93628491c7b27e4e62065e
Carlota-pg <carlota.pinedo03@gmail.com>
2025-05-31 15:37:19 +0200

    torre 2

--- Ajedrez/src/Torre.cpp
+++ Ajedrez/src/Torre.cpp
@@ -7,27 +7,39 @@
 
 void Torre::mueveficha(double, double)
 {
-
+	posicion_ficha.x = x;
+	posicion_ficha.y = y;
 }
 
 void Torre::set_pos_ficha(const Vector2D& pos)
 {
-
+	posicion_ficha = pos;
 }
 
 void Torre::dibuja_ficha()
 {
-
+	glPushMatrix();
+	glTranslated(posicion_ficha.x, posicion_ficha.y, 0.001);
+	if (color == 0) 
+		sprite2.draw();
+	if (color == 1)
+		sprite.draw();
 }
 
 void Torre::set_color_ficha(bool a)
 {
-
+	if (a == true)
+		color = true; //1
+	if (a == false)
+		color = false;
 }
 
 bool Torre::FichaAhogada(vector<Vector2D> posiciones)
 {
-	return false;
+	if (posiciones.size() == 0)
+		return true;
+	else
+		return false;
 }
 
 std::vector<Vector2D> Torre::get_movimientos_validos(std::vector<std::vector<Fichas*>> control, Vector2D pos, Vector2D reyPos)
@@ -37,5 +49,8 @@ std::vector<Vector2D> Torre::get_movimientos_validos(std::vector<std::vector<Fic
 
 bool Torre::casillaValida(int i, int j, std::vector<std::vector<Fichas*>> control)
 {
-	return false;
+	if (i >= 0 && i < control.size() && j >= 0 && j < control[0].size())
+		return true;
+	else
+		return false;
 }
